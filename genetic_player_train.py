f9b30d5d1ccaf030848e533985e5eb0857730bc5
Stephane_Branly <stephanebranly.pro@gmail.com>
2022-09-06 12:50:32 -0400

    [-] Fix crossover

--- genetic_player_train.py
+++ genetic_player_train.py
@@ -132,7 +132,6 @@ class MyAgent(Agent):
                 self.scores[self.current_match[1-player_winner]] -= abs(winner)
             if len(self.matchs):
                 self.load_match()
-        # print(self.scores)
 
     def load_match(self):
         self.current_match = self.matchs.pop(0)
@@ -148,9 +147,9 @@ class MyAgent(Agent):
             f.close()
             for l in range(self.nb_individu):
                 father = NN([9,10,8])
-                father.load_from_json(f"NN/gen{self.gen}.json", results[random.randint(0, len(self.nb_individu)//10)][0])
+                father.load_from_json(f"NN/gen{self.gen}.json", results[random.randint(0, self.nb_individu//10)][0])
                 mother = NN([9,10,8])
-                mother.load_from_json(f"NN/gen{self.gen}.json", results[random.randint(0, len(self.nb_individu)//10)][0])
+                mother.load_from_json(f"NN/gen{self.gen}.json", results[random.randint(0, self.nb_individu//10)][0])
                 child = father.crossover(mother)
                 child.mutate()
                 
