ff627d1885c9b14d27d65b10e4a0b20a3d511706
Guichard Amaury <reven.myst@gmail.com>
2022-11-20 00:19:27 +0100

    sah

--- test.py
+++ test.py
@@ -1,22 +1,27 @@
+#!/usr/bin/env python3
+
 from avalam import *
-import random
+import json
+from heuristic.GeneticBoardEvaluation import GeneticBoardEvaluation
 
-b = ImprovedBoard(compute_isolated_towers=True)
 
-def print_data(b):
-    print(b)
-    sum = 0
-    for key in b.addable_towers:
-        sum += b.addable_towers[key]
-        print(f"{key}\t: {int(b.addable_towers[key])}")
-    print(len(list(b.get_actions())))
-print_data(b)
-actions=list(b.get_actions())
-while len(actions) > 18:
-    actions = list(b.get_actions())
-    b.play_action(random.choice(actions))
-    if random.random() < 0.5:
-        b.undo_action()
-        print("undo")
+def load_best_individu(gen,h):
+    try:
+        with open(f"GeneticAgents/links/gen{gen}.json") as fp:
+            listObj = json.load(fp)
+        scores = [cell['score'] for cell in listObj['gen']]
+        individu = scores.index(max(scores))
+        h.load_from_json(f"GeneticAgents/links/gen{gen}.json", individu)
+    except BaseException as e:
+        print(f"BaseException raised: {e}")
+        print('No more generation')
 
-print_data(b)
+# main function
+if __name__ == "__main__":
+
+    h = GeneticBoardEvaluation()
+
+
+    for i in range(50):
+        load_best_individu(i+352,h)
+        h.save_as_json(f"GeneticAgents/links/gen{999}.json", 0)
