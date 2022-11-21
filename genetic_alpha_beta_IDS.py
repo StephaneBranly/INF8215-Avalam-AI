0ff44344f7df9a392ba6ea747e855c99afc86e36
Guichard Amaury <reven.myst@gmail.com>
2022-11-21 20:25:55 +0100

    fix alpha beta

--- genetic_alpha_beta_IDS.py
+++ genetic_alpha_beta_IDS.py
@@ -29,15 +29,16 @@ import math
 class MyAgent(Agent):
 
     def __init__(self):
-        self.trimming_ratios = {6:{2:4.43,3:28.68},8:{2:3.62,3:32.08},10:{2:3.62,3:27.12},12:{2:4.33,3:26.17,4:4.32},13:{2:2.94},14:{2:3.77,3:22.14,4:3.82},15:{2:4.97},16:{2:4.0,3:18.92,4:2.62,5:9.13},17:{2:3.53},18:{2:3.48,3:17.09,4:3.9,5:6.9},20:{2:3.65,3:16.19,4:3.36,5:6.65},21:{2:4.0,3:10.47},22:{2:3.24,3:11.12,4:2.92,5:5.91,6:2.84,7:3.76},23:{2:3.56,3:9.05,4:2.51},24:{2:3.27,3:9.0,4:2.32,5:5.2,6:2.36,7:2.4},25:{2:5.26,3:6.77,4:2.56},26:{2:3.14,3:6.09,4:2.41,5:3.47,6:1.63,7:1.87,8:0.43},27:{2:3.73,3:4.5,4:2.31,5:3.8,6:1.19,7:2.04},28:{2:2.84,3:4.02,4:1.87,5:2.02,6:1.16,7:0.75,8:0.85},29:{2:2.57,3:3.03,4:1.97,5:2.35,6:2.57,7:0.87,8:1.06},30:{2:2.35,3:2.18,4:1.23,5:0.89,6:0.95,7:0.99,8:1.0},31:{2:2.43,3:2.35,4:1.42,5:0.76,6:1.17,7:1.03,8:0.91},32:{2:1.39,3:0.84,4:0.93,5:0.97,6:1.0,7:0.88,8:0.68},33:{2:1.08,3:1.22,4:0.84,5:1.05,6:1.05,7:0.93,8:0.91},34:{2:0.87,3:0.72,4:0.98,5:0.6,6:0.5,7:0.88,8:1.37},36:{2:0.57,3:0.87,4:1.0,5:1.0}}
+        self.trimming_ratios = {6:{2:4.43,3:28.68},8:{2:3.62,3:32.08},10:{2:3.62,3:27.12},12:{2:4.33,3:26.17,4:4.32},13:{2:2.94},14:{2:3.77,3:22.14,4:3.82},15:{2:4.97},16:{2:4.0,3:18.92,4:2.62,5:9.13},17:{2:3.53},18:{2:3.48,3:17.09,4:3.9,5:6.9},20:{2:3.65,3:16.19,4:3.36,5:6.65},21:{2:4.0,3:10.47},22:{2:3.24,3:11.12,4:2.92,5:5.91,6:2.84,7:3.76},23:{2:3.56,3:9.05,4:2.51},24:{2:3.27,3:9.0,4:2.32,5:5.2,6:2.36,7:2.4},25:{2:5.26,3:6.77,4:2.56},26:{2:3.14,3:6.09,4:2.41,5:3.47,6:1.63,7:1.87,8:0.43},27:{2:3.73,3:4.5,4:2.31,5:3.8,6:1.19,7:2.04},28:{2:2.84,3:4.02,4:1.87,5:2.02,6:1.16,7:0.75,8:0.85},29:{2:2.57,3:3.03,4:1.97,5:2.35,6:2.57,7:0.87,8:1.06},30:{2:2.35,3:2.18,4:1.23,5:0.89,6:0.95,7:0.99,8:1.0},31:{2:2.43,3:2.35,4:1.42,5:0.76,6:1.17,7:1.03,8:0.91}}
 
         self.heuristic_core = heuristic()
         super().__init__()
 
-    def need_trimming(self,step,depth,start,time_to_play,last_time):
+    # on verifie si on a le temps de calculer la prochaine profondeur en multipliant le temps necessaire pour la profondeur courante par un ratio trouvé empiriquement.
+    def need_trimming(self,step,depth,start,time_to_play,last_time) -> bool:
         time_remaining = time_to_play - (time.time() - start)
         if depth > 1 and depth <= 8:
-            for i in range(step,37):
+            for i in range(step,32):
                 if(i in self.trimming_ratios and depth in self.trimming_ratios[i]):
                     return self.trimming_ratios[i][depth] * last_time > time_remaining
         return False
@@ -47,17 +48,19 @@ class MyAgent(Agent):
         print("step: ", step)
 
         start = time.time()
+
+        # on determine le temps du coup, une partie fait en moyenne 34-35 coups donc on divise le temps restant par le nombre de step a jouer
         time_to_play = time_left/((34-step)/2) if step < 34 else time_left/2
+
+        # step charnieres, grace au trimming on fini la partie avec "trop de temps" ici on augmente le temps alloue car ca permet de visiter une profondeur supplementaire
         if player == 1 and step > 17 and step < 25:
-            time_to_play *= 2
+            time_to_play *= 1.75
         print(time_left, time_to_play)
+
+        # voir improved board
         board = dict_to_improved_board(percepts,True)
 
-        actions = [(4, 2, 4, 3),[3, 1, 4, 0],[4, 0, 4, 1],[6, 2, 5, 1],[5, 1, 5, 0],[5, 2, 4, 1],[4, 3, 5, 4],[6, 4, 5, 3],[5, 3, 5, 4],[8, 6, 7, 7],[7, 7, 7, 6],[3, 2, 3, 3],[3, 3, 3, 4],[3, 4, 2, 4],[3, 5, 2, 4],[4, 6, 5, 5],[5, 5, 5, 6],[3, 8, 4, 8],[5, 7, 4, 8],[7, 5, 6, 6],[6, 6, 6, 7],[8, 5, 7, 6],[0, 2, 0, 3],[2, 3, 1, 3],[2, 6, 2, 5],[1, 4, 2, 5],[2, 2, 1, 3]]
-        if step <28:
-            return actions[step-1]
-        time_to_play = 7700
-
+        # pour economiser du temps, on n'explore que la profondeur 1 pour les 5 premiers coups de la partie
         if step <= 5:
             max_depth = 1
         else:
@@ -66,20 +69,26 @@ class MyAgent(Agent):
         depth = 0
         last_time = 0
 
+        #tant qu'il nous reste du temps qu'on a pas parcouru tte les depth autorisé et qu'il reste assez de temps pour parcourir la depth suivante (voir trimming)
         while time.time()-start<time_to_play and depth<max_depth and not self.need_trimming(step,depth,start,time_to_play,last_time):
 
             new_start = time.time()
             depth += 1
+
+            #initialisation des hashmaps qui stocke les etats visités
             hashMaps = []
             for i in range(0, 40):
                 hashMaps.append({})
+            
+            # visite jusqu'a une certaine profondeur
             v, m = self.max(board, player, -math.inf, math.inf, 0, depth, hashMaps, start, time_to_play)
-            print("depth: ", depth, "value: ", v, "time: ", time.time()-new_start)
+
+
             last_time = time.time()-new_start
+            # si on a le temps de parcourir la profondeur en entier on stocke l'action retournée
             if(time.time()-start<time_to_play or depth==1):
                 action = m
 
-        print(action)
         return action
 
     def min(self, board, player, alpha, beta, depth, max_depth, hash_maps, start, time_to_play):
@@ -135,6 +144,7 @@ class MyAgent(Agent):
 
 
     def save_hash(self, board, depth, hash_maps, v, m):
+        # NB si deux etats sont identiques, ils sont forcement à la meme step/depth
         if depth > 1:
             hash = board.get_hash()
             hash_maps[depth][hash] = (v,m)
